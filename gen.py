a01f4714413b7317e06104de61310db8d015b677
Adel NOUAR <adel.nouar@protonmail.com>
2022-02-14 13:55:38 +0100

    TODOs

--- gen.py
+++ gen.py
@@ -1,3 +1,35 @@
+'''
+TODOs: 
+- Test for copy part: 
+        --> folders copied in the jupyter starter and final folders
+        --> notebooks copied in the jupyter starter and final folders
+        --> other files copied in the jupyter starter and final folders
+
+- Test all code cells in starter notebooks cleared
+
+- Modify the add_header_colab function:
+        --> Take GIT_HUB_PATH constant, and concatenate with the folder name (only main folder project name + subfolders (colab/starer and colab/final)).
+        
+        --> Create a text represent the google colabs link with the constant above.
+
+        --> Create a cell with the previous text.
+
+        --> add the entire notebook to that cell.
+
+        --> save the notebook filename (bye overriding it with the new content).
+
+- Instead of given list of folders in the command arguments:
+        --> An input file with the list of all repos to modify
+        --> for each repos: 
+                            ---> git clone
+                            ---> git checkout -b dev
+                            ---> performs the actions
+                            ---> git push
+
+- When all things work, cleaning the code, and using OOP, each repos will be an object.
+
+'''
+
 from distutils.file_util import copy_file
 from genericpath import isfile
 import sys
@@ -10,6 +42,7 @@ from shutil import copytree
 from shutil import copyfile
 
 
+GIT_HUB_PATH = "https://github.com/LearnPythonWithRune/"
 
 current_path:str = ""
 
